1a845535cca108a72859c71dc03d0c317ed2e980
Astro <astro@spaceboyz.net>
2022-12-11 01:08:27 +0100

    error: use thiserror

--- src/error.rs
+++ src/error.rs
@@ -3,36 +3,34 @@ use std::string::FromUtf8Error;
 use http::header::{ToStrError, InvalidHeaderValue};
 
 /// General error type
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// Missing a field in the `Signature:` header
+    #[error("Missing a field in the `Signature:` header")]
     MissingField(&'static str),
     /// `Signature:` header is missing
+    #[error("`Signature:` header is missing")]
     SignatureHeaderMissing,
     /// Cannot use invalid bytes as HTTP header value
+    #[error("Cannot use invalid bytes as HTTP header value")]
     HeaderValue(ToStrError),
     /// Cannot serialize HTTP header
+    #[error("Cannot serialize HTTP header")]
     SerializeHeader(InvalidHeaderValue),
     /// Signature algorithm not implemented
+    #[error("Signature algorithm not implemented")]
     UnknownAlgorithm(String),
     /// Error parsing the `Signature:` header
+    #[error("Error parsing the `Signature:` header")]
     ParseSignatureHeader(nom::Err<nom::error::Error<String>>),
     /// Cannot decode base64
+    #[error("Cannot decode base64")]
     SignatureBase64(base64::DecodeError),
     /// Cryptographic issue
-    OpenSsl(openssl::error::ErrorStack),
+    #[error("Cryptographic issue")]
+    OpenSsl(#[from] openssl::error::ErrorStack),
     /// Invalid UTF-8
-    Utf8(FromUtf8Error),
+    #[error("Invalid UTF-8")]
+    Utf8(#[from] FromUtf8Error),
 }
 
-impl From<openssl::error::ErrorStack> for Error {
-    fn from(e: openssl::error::ErrorStack) -> Self {
-        Error::OpenSsl(e)
-    }
-}
-
-impl From<FromUtf8Error> for Error {
-    fn from(e: FromUtf8Error) -> Self {
-        Error::Utf8(e)
-    }
-}
